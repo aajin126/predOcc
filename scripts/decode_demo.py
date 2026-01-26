a3efd5d944f60dd7fd7e75fbcc3477c6c1461920
HahJinLee <122766068+aajin126@users.noreply.github.com>
2026-01-26 14:47:13 +0900

    test v2

--- scripts/decode_demo.py
+++ scripts/decode_demo.py
@@ -141,7 +141,7 @@ def main(argv):
     # for each batch in increments of batch size:
     counter = 0
     all_rows = []       
-    csv_path = os.path.join("output", "v2.0", "eval_table.csv")
+    csv_path = os.path.join("output", "v2.1", "eval_table.csv")
     # get the number of batches (ceiling of train_data/batch_size):
     num_batches = int(len(eval_dataset)/eval_dataloader.batch_size)
     with torch.no_grad():
@@ -226,7 +226,7 @@ def main(argv):
 
             for k in range(SEQ_LEN):  
                 prediction, kl_loss = model(inputs_samples, inputs_occ_map_samples)
-                prediction_t, _ = reprojection(prediction, x_rel[:, k], y_rel[:, k], th_rel[:, k], MAP_X_LIMIT, MAP_Y_LIMIT)
+                prediction_t, valid_mask = reprojection_logits(prediction, x_rel[:, k], y_rel[:, k], th_rel[:, k], MAP_X_LIMIT, MAP_Y_LIMIT)
                 prediction = prediction.reshape(-1,1,1,IMG_SIZE,IMG_SIZE)
                 prediction_t = prediction_t.reshape(-1,1,1,IMG_SIZE,IMG_SIZE)
                 inputs_samples = torch.cat([inputs_samples[:,1:], prediction], dim=1)
@@ -235,9 +235,11 @@ def main(argv):
                 predictions_origin = prediction.squeeze(1)
 
                 pred_mean = torch.mean(predictions, dim=0, keepdim=True)
+                pred_mean = torch.sigmoid(pred_mean) * valid_mask[0]
                 prediction_maps[k, 0] = pred_mean.squeeze()
 
                 pred_mean_origin = torch.mean(predictions_origin, dim=0, keepdim=True)
+                pred_mean_origin = torch.sigmoid(pred_mean_origin)
                 prediction_maps_org[k, 0] = pred_mean_origin.squeeze()
 
             # end timing
@@ -270,7 +272,7 @@ def main(argv):
                 fontsize = 8
                 input_title = "n=" + str(m+1)
                 a.set_title(input_title, fontdict={'fontsize': fontsize})
-            input_img_name = "./output/v2.0/mask" + str(i)+ ".jpg"
+            input_img_name = "./output/v2.1/mask" + str(i)+ ".jpg"
             plt.savefig(input_img_name)
             plt.close(fig)
 
@@ -286,7 +288,7 @@ def main(argv):
                 plt.yticks([])
                 input_title = "n=" + str(m+1)
                 a.set_title(input_title, fontdict={'fontsize': fontsize})
-            input_img_name = "./output/v2.0/pred" + str(i)+ ".jpg"
+            input_img_name = "./output/v2.1/pred" + str(i)+ ".jpg"
             plt.savefig(input_img_name)
             plt.close(fig)
 
