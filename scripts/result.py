845b42eee36afdeb17c56d140ea7f9c05d01a0af
aajin126 <leehahj77@gmail.com>
2026-04-05 16:32:24 +0900

    eval

--- scripts/result.py
+++ scripts/result.py
@@ -6,14 +6,15 @@ import matplotlib.pyplot as plt
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
 files = {
-    #"v1.6": os.path.join(ROOT, "output", "v1.6", "eval_table.csv"),
-    #"v7.7.1": os.path.join(ROOT, "output", "v7.7.1", "eval_table.csv"),
-    "v6.6.0": os.path.join(ROOT, "output", "v6.6.0", "eval_table.csv"),
-    #"ldm2.0": os.path.join(ROOT, "output", "ldm2.0", "eval_table.csv"),
-    "ldm2.1": os.path.join(ROOT, "output", "ldm2.1", "eval_table.csv"),
+    "v1.6": os.path.join(ROOT, "output", "v1.6", "eval_table.csv"),
+    "v1.7": os.path.join(ROOT, "output", "v1.7", "eval_table.csv"),
+    #"v7.7.0": os.path.join(ROOT, "output", "v7.7.0", "eval_table.csv"),
+    # "v6.6.0": os.path.join(ROOT, "output", "v6.6.0", "eval_table.csv"),
+    # "ldm2.0": os.path.join(ROOT, "output", "ldm2.0", "eval_table.csv"),
+    # "ldm2.1": os.path.join(ROOT, "output", "ldm2.1", "eval_table.csv"),
     #"v6.2.0.1": os.path.join(ROOT, "output", "v6.2.0.1", "eval_table.csv"),
     #"v6.2.0": os.path.join(ROOT, "output", "v6.2.0", "eval_table.csv"),
-    #"SOGMP++": os.path.join(ROOT, "output", "sogmp++","eval_table.csv"),
+    "SOGMP++": os.path.join(ROOT, "output", "sogmp++","eval_table.csv"),
 }
 
 
@@ -39,7 +40,7 @@ plt.grid(True, linestyle="--", linewidth=0.5, alpha=0.6)
 plt.legend()
 plt.tight_layout()
 
-out_png = os.path.join(ROOT, "output", "iou_eval_plot_9.png")
+out_png = os.path.join(ROOT, "output", "iou_eval_plot_12.png")
 plt.savefig(out_png, dpi=300)
 plt.show()
 print("Saved:", out_png)
